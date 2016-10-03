e4854ef98094518c5391a4382a2da0159dc040ef
Jason Callaway <jason@jasoncallaway.com>
2016-10-03 00:11:59 -0400

    cleaned up playbook, added instance filtering logic

--- run.sh
+++ run.sh
@@ -1,22 +1,11 @@
 #!/usr/bin/env bash
 
-if [ ${#} -ne 1 ]
-then
-    echo "specify number of instances"
-    exit 1
-fi
-
-printf "Refreshing EC2 cache..."
-./inventory/aws/hosts/ec2.py --refresh-cache
-printf "done\n"
-
 ssh-add -l >/dev/null 2>&1
 if [ "${?}" == "0" ]
 then
-    ansible-playbook -v -i inventory/aws/hosts/ec2.py \
-                     --extra-vars "instances=${1}" \
-                     workshop.yaml
+  ansible-playbook -v -i "localhost" localhost.yml && \
+  ansible-playbook -v -i inventory/aws/hosts/ec2.py workshop.yml
 else
-    echo "You must run this with ssh-agent holding a key via ssh-add"
-    exit 1
+  echo "You must run this with ssh-agent holding a key via ssh-add"
+  exit 1
 fi
